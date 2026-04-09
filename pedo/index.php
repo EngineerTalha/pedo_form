c1d50624790e3ac9fd0abc388545090bd958dd6e
TalhaEngineer <ahmadtalhamad3@gmail.com>
2026-04-09 12:36:45 +0500

    login button centered

--- pedo/index.php
+++ pedo/index.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>PEDO | Application Portal</title>
-  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
   <div class="landing-container">
